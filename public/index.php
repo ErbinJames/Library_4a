faf17fc913c60af907ffea492d0375df2aae050b
ErbinJames <eorodio04922@student.dmmmsu.edu.ph>
2024-12-03 02:13:58 +0800

    updating index

--- public/index.php
+++ public/index.php
@@ -397,7 +397,7 @@ $app->post('/author/register', function (Request $request, Response $response, a
     $username = "root";
     $password = "";
     $dbname = "library";
-
+    
     try {
         $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
